b8d14975117c2d5d6c412616d5d460d7457cf9b9
Caleb Redd <caleb.redd@yahoo.com>
2019-08-27 17:41:26 -0700

    Token API sorted out with interceptors.js. Pulling user list from API with data.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import logo from './logo.svg';
 import "./App.css";
 import AppRoute from "./components/AppRoute";
 import Nav from "./components/Nav";
+import axios from "axios";
 // import SignUp from './components/SignUp';
 // import Hairstylist from './components/Hairstylist';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://haircare-backend.herokuapp.com/api/users")
+      .then(res => {
+        console.log("res: ", res);
+        setUsers(res.data.users);
+      })
+      .catch(err => {
+        console.error("handleSubmit: catch: err: ", err);
+      });
+  }, []);
   return (
     <div className="App container">
       <Nav />
       <header />
       <div>
         <AppRoute />
+      </div>
+      <div className="user-info">
+        <h1>Users:</h1>
+        {users.map(user => (
+          <div className="user-card-info">
+            <ul key={user.id}>
+              <li>Username: {user.username}</li>
+              <li>Id: {user.id} </li>
+              <li>User Id: {user.user_id} </li>
+              <li>Image url: {user.profile_img}</li>
+              <li>About: {user.about}</li>
+              <li>Skills: {user.skills}</li>
+            </ul>
+            <br />
+            <br />
+          </div>
+        ))}
       </div>
     </div>
   );
